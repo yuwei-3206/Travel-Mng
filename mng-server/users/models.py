4e0696900230238e5719c19047d9ab179e2b7774
yuwei-3206 <122844465+yuwei-3206@users.noreply.github.com>
2024-06-06 15:06:04 -0400

    Update models.py

--- mng-server/users/models.py
+++ mng-server/users/models.py
@@ -4,5 +4,5 @@ class User(db.Model):
     id = db.Column(db.String(36), primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
